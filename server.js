dcc7f54cb9cc82e1483e275e5d40698f1c05b36d
Kritika Singh <kritikasingh273013@gmail.com>
2025-09-18 13:49:50 +0530

    cors

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ dotenv.config(); // <-- keep it simple
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",   // frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 import userRoutes from "./routes/userRoutes.js";
